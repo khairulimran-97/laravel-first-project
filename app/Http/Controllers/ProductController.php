08e70825493e51774fa8592a61691d9a2ae420eb
Khairul <info@khairulimran.com>
2022-05-10 05:24:24 +0800

    logout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Cart;
 use Illuminate\Http\Request;
+use Session;
 
 class ProductController extends Controller
 {
@@ -33,6 +34,11 @@ class ProductController extends Controller
         {
             return redirect('login');
         }
+    }
+    static function cartItem()
+    {
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
     
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,12 @@
+<?php
+use App\Http\Controllers\ProductController;
+$total=0;
+if(Session::has('user'))
+{
+  $total=ProductController::cartItem();
+}
+
+?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
@@ -8,23 +17,34 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Brand</a>
+        <a class="navbar-brand" href="#">Khairul Store</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home </a></li>
+          <li class="active"><a href="/">Home </a></li>
           <li class=""><a href="#">Order </a></li>
         </ul>
-        <form class="navbar-form navbar-left">
+        <form action="/search" class="navbar-form navbar-left">
           <div class="form-group">
             <input type="text" class="form-control" placeholder="Search">
           </div>
           <button type="submit" class="btn btn-default">Submit</button>
         </form>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Cart(0)</a></li>
+          <li><a href="#">cart({{$total}})</a></li>
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/Login">Login</a></li>
+          @endif
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
